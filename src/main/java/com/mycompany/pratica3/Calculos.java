d05169b345711729d755fcb18ef8fdf6ce25f34f
Fernanda Nunes Oliveira Santos <fernandanos@hotmail.com>
2024-03-17 12:17:46 -0300

    Passando raiva

--- src/main/java/com/mycompany/pratica3/Calculos.java
+++ src/main/java/com/mycompany/pratica3/Calculos.java
@@ -8,6 +8,9 @@ package com.mycompany.pratica3;
  *
  * @author Fernanda
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculos {
     public static float CalculaMedia(int  idade[]){
         float soma=0;
@@ -16,4 +19,24 @@ public class Calculos {
         }
         return soma/idade.length;
     }
-}
+    public static List<Integer> MostraImpar(int impar){ //Criando um método do tipo List<Integer>
+        List <Integer> impares=new ArrayList<>(); // Instaciando Objeto 
+        for(int i=1;i<=impar;i++){
+            if (i%2!=0){
+                impares.add(i); // adicionando cada número impar ao array impares
+            }
+        }
+        return impares; // Retoando a lista
+        // Tentei criar um vetor int vetor[]=new int[impar.lenght]; para testar e não funcionou. Acredito que o método em questão precisa ser do tipo Lista 
+    }
+    public static int Verificao(float peso){
+        for(int i=1;i<=peso;i++){
+            if(peso>=90){
+            }
+        }
+        
+        }
+
+    }
+    
+
--- src/main/java/com/mycompany/pratica3/Pratica3.java
+++ src/main/java/com/mycompany/pratica3/Pratica3.java
@@ -11,8 +11,8 @@ package com.mycompany.pratica3;
 import java.util.Scanner;
 public class Pratica3 {
     static Scanner input=new Scanner(System.in);
-    public static void main(String args[]) {
-        
+    public static void main(String args[], float salvaPeso) {
+        /*
         System.out.println("Faça um programa que leia a idade de 10 pessoas, calcule e mostre a média de idade das mesmas.");
         int idade[]=new int [10];
         idade[0]=input.nextInt();
@@ -25,8 +25,44 @@ public class Pratica3 {
         idade[7]=input.nextInt();
         idade[8]=input.nextInt();
         idade[9]=input.nextInt();
-        
         System.out.println(Calculos.CalculaMedia(idade));
-    }
+        
+        System.out.println("Faça um programa que leia um número e que imprima os números ímpares de 1 até o número informado");
+        int impar=input.nextInt();
+        System.out.println(Calculos.MostraImpar(impar));
+        */
+        System.out.println("""
+                           Fa\u00e7a um programa que leia a idade e peso de sete pessoas. Calcule e mostre:
+                           \t- a quantidade de pessoas com mais de 90 kg
+                           \t- a m\u00e9dia das idades das sete pessoas""");
+        int idade[]=new int[7];
+        float peso[]= new float[7];
+        System.out.println("Peso e idade pessoa 1: ");
+        idade[0]=input.nextInt();
+        peso[0]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 2: ");
+        idade[1]=input.nextInt();
+        peso[1]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 3: ");
+        idade[2]=input.nextInt();
+        peso[2]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 4: ");
+        idade[3]=input.nextInt();
+        peso[3]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 5: ");
+        idade[4]=input.nextInt();
+        peso[4]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 6: ");
+        idade[5]=input.nextInt();
+        peso[5]=input.nextFloat();
+        System.out.println("Peso e idade pessoa 7: ");
+        idade[6]=input.nextInt();
+        peso[6]=input.nextFloat();
+     
+        System.out.println(Calculos.CalculaMedia(idade));
+        
+        
+        System.out.println(Calculos.VerificaPeso(peso));
+        }
 
 }
